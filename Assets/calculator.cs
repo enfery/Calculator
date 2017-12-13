64e01c866321c15bfe3b5bd4816e2d08c657b59a
enfery <enfery.demon@mail.ru>
2017-12-13 22:22:26 +0300

    init

---
+++ Assets/calculator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class calculator : MonoBehaviour {
+
+	public void ButtonPressed()
+	{
+		Debug.Log (EventSystem.current.currentSelectedGameObject.name);
+	}
+
+	public void ApplicationExit()
+	{
+		Application.Quit ();
+	}
+}
