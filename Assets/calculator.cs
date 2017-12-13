9110574ab042fc4a91b513d2bd7642ed42e4b1f8
enfery <enfery.demon@mail.ru>
2017-12-13 23:05:09 +0300

    enter,clear

--- Assets/calculator.cs
+++ Assets/calculator.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class calculator : MonoBehaviour {
+
+	[SerializeField]
+	Text outputTextField;
+	private string buttonValue;
 
 	public void ButtonPressed()
 	{
 		Debug.Log (EventSystem.current.currentSelectedGameObject.name);
+		buttonValue = EventSystem.current.currentSelectedGameObject.name;
+		outputTextField.text += buttonValue;
 	}
 
 	public void ApplicationExit()
 	{
 		Application.Quit ();
+	}
+
+	public void ClearOut()
+	{
+		outputTextField.text = "";
 	}
 }
